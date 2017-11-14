97086382522250944224558021c1e3c744f072d5
shoaf44 <32044203+shoaf44@users.noreply.github.com>
2017-11-14 11:24:20 -0500

    Added similarity algorithm for two plain text passwords

--- app/compareOldToNewPassword.py
+++ app/compareOldToNewPassword.py
@@ -1,17 +1,27 @@
 import collections
 import sys
 
-
-
-
-
-
-
-
-#def newPasswordToOldPasswordComparison(newPassword, oldPassword):
-    newPassword = sys.argv[1]
-    oldPassword = sys.argv[2]
+#def main():
+def newPasswordToOldPasswordComparison(newPassword, oldPassword):
+#    oldPassword = sys.argv[1]
+#    newPassword = sys.argv[2]
+    newPassword = newPassword.lower()
+    oldPassword = oldPassword.lower()
     newPasswordLetters = collections.Counter(newPassword)
     oldPasswordLetters = collections.Counter(oldPassword)
-    print newPasswordLetters
-    print oldPasswordLetters
+    intersection = newPasswordLetters & oldPasswordLetters
+    print intersection
+    count = 0 
+    for item in intersection:
+        count = count + intersection[item]
+    percent = float(count)/len(newPassword)
+    print percent
+    if (percent > .7):
+#        print False
+        return False
+    else:
+#        print True
+        return True
+
+#if __name__ == "__main__":
+#    main()
