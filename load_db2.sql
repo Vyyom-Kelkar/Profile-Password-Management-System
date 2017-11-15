5f7719944c6cada2ec32c424de58f18d53e8ae51
bboehmer <boehmer.30@osu.edu>
2017-11-14 19:20:24 -0500

    Created second load db script

---
+++ load_db2.sql
@@ -0,0 +1,12 @@
+INSERT INTO Admin_Setting values
+('GM',15,1,1,1,1,30);
+
+INSERT INTO User values
+('A','catty',1,'thisisnotemail2@email.com','Bad Company','666-666-6669','1403-03-11 02:01:04.99383','akjv;asv;av;fadsfsdfv','1996-02-13 10:08:08.174');
+
+INSERT INTO Old_Password values
+('thisisnotemail2@email.com', 'sportsball'),
+('thisisnotemail2@email.com', 'hailsatan');
+
+INSERT INTO Common_Password values
+('passwordddddd'),
