3b7ccfd106fa8cc0f8391e6e584ed2521ca51baa
bboehmer <boehmer.30@osu.edu>
2017-11-14 19:15:26 -0500

    Fixed Load_db file

--- load_db.sql
+++ load_db.sql
@@ -8,10 +8,10 @@ INSERT INTO User values
 ('A','catty',1,'thisisnotemail@email.com','Bad Company','666-666-6669','1403-03-11 02:01:04.99383','akjv;asv;av;fadsfsdfv','1996-02-13 10:08:08.174');
 
 INSERT INTO Old_Password values
-(thisisemail@email.com, 'sportsball'),
-(thisisemail@email.com, 'computers'),
-(thisemail@email.com, 'ihatemen'),
-(thisisnotemail@email.com, 'hailsatan');
+('thisisemail@email.com', 'sportsball'),
+('thisisemail@email.com', 'computers'),
+('thisemail@email.com', 'ihatemen'),
+('thisisnotemail@email.com', 'hailsatan');
 
 INSERT INTO Common_Password values
 ('password'),
